4d583b2bb63423afc0ade49b321d8a24e8660681
Rahul Verma <rahul281191@gmail.com>
2022-10-09 23:26:23 +0530

    First Commit

---
+++ src/components/MainPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const MainPage = () => {
+  return (
+    <div className="content">
+        <Navbar />
+    </div>
+  )
+}
+
+export default MainPage
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+
+
+const Navbar = () => {
+  return (
+    <nav>
+    
+        <div className="logo">
+          <img src="../logo.jpg" alt="site logo" width={40} height={30} />
+          <span className="logo-text">Book My Ticket</span>
+        </div>
+     <div className="home">Home</div>
+    </nav>
+  )
+}
+
+export default Navbar
